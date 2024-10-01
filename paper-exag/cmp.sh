9d6a32d31e4dc7b5b43954b9d20d761e9c412229
zzyzek <zzyzek@thumpernet.com>
2024-10-01 01:13:34 -0400

    wip
    * results section needs a lot of work

--- paper-exag/cmp.sh
+++ paper-exag/cmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f *.log *.aux *.abs *.log *.out *.xmpdata *.xmpi *.bbl *.blg
-pdflatex --text-option=--shell-escape POMS.tex
+pdflatex POMS.tex
 bibtex POMS
 pdflatex POMS.tex
 pdflatex POMS.tex
